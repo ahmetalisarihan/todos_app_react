073a8c83f393c0971cfd848c46ddbcea97115a36
ahmetalisarihan <ahmetalisarihan@gmail.com>
2023-02-19 01:28:42 +0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,38 @@
 import { useState, useEffect } from "react";
-import React from 'react'
 
-function Header() {
+function Header({ todos, setTodos }) {
+  const [form, setForm] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (form === "") {
+    }
+
+    setTodos([
+      ...todos,
+      {
+        id: todos.lenght > 0 ? todos[todos.lenght - 1].id + 1 : 0,
+        todo: form,
+        checked: false,
+      },
+    ]);
+  };
+
   return (
-    <div>Header</div>
-  )
+    <header className="header">
+      <h1>todos</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus
+          value={form}
+          onChange={(e) => setForm(e.target.value)}
+        />
+      </form>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
